33a918f1cef36e99ec1e6375ba5078cd39276181
zbenkhelifa <zahire.benkhelifa@ac-versailles.fr>
2026-09-13 14:02:42 +0000

    ci: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/ModuleCours/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
